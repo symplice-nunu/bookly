7f0791ef4be1a29b551330e454d20e8338c096e9
Symplice <intwarisymplice@gmail.com>
2023-07-06 01:45:55 +0200

    feat: adding hotel details on hotel page

--- src/Components/Hotels.js
+++ src/Components/Hotels.js
@@ -4,10 +4,12 @@ import dining from '../assets/booklimages/images/hotel_dining room.jpg'
 import weddingroom from '../assets/booklimages/images/hotel_bedroom.jpg'
 import garden from '../assets/booklimages/images/hotel_gym.jpg'
 import swimmingpool from '../assets/booklimages/images/hotel_swimming_pool.jpeg'
+import HotelSelection from './HotelSelection'
 
 export default function Hotels() {
   return (
-    <div className='flex gap-1 w-full px-[170px] pb-12'>
+    <div>
+      <div className='flex gap-1 w-full px-[170px] pb-12'>
       <div><img className='rounded-xl h-full' src={bedroom} /></div>
       <div className='flex gap-1'>
         <div className='space-y-1'>
@@ -19,6 +21,8 @@ export default function Hotels() {
           <div className='w-[250px] rounded'><img className='rounded h-[205px] object-cover' src={weddingroom} /></div>
         </div>
       </div>
+    </div>
+    <HotelSelection />
     </div>
   )
 }
