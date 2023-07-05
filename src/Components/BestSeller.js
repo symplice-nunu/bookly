75147de91983012db1bb7b7ea96ab85d6b2af04a
symplice-nunu <intwarisymplice@gmail.com>
2023-07-05 18:37:08 +0530

    fix: fixing best seller alignment issue

--- src/Components/BestSeller.js
+++ src/Components/BestSeller.js
@@ -28,7 +28,7 @@ export default function BestSeller() {
                 BestSellerData.map((item) => {
                     return(
                         <div className='bg-white rounded-3xl  pb-[40px]'>
-                            <div><img className='rounded-3xl w-[510px] h-[300px] object-cover' src={item.photo} /></div>
+                            <div><img className='rounded-3xl w-full h-[300px] object-cover' src={item.photo} /></div>
                             <div className='flex justify-between px-6 mt-1'>
                             <div className='flex space-x-2'>
                             <div className='text-[#9a9a9a]'>{item.rate}</div>
