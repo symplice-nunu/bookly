460f0b7b54ff32d9a1f0237f8a47108785135324
symplice-nunu <intwarisymplice@gmail.com>
2023-07-06 18:18:37 +0530

    feat: adding recommended hotels tabs

--- src/Components/RecommendedHotel.js
+++ src/Components/RecommendedHotel.js
@@ -9,44 +9,59 @@ export default function RecommendedHotel() {
   return (
     <div>
         <div className='text-[30px] my-[20px] font-bold'>Other Recommended Hotel</div>
-        <div className='flex gap-24'>
-          <div></div>
-          <div></div>
-          <div>Most popular hotels</div>
-        </div>
         <div>
       <div className="flex">
         {/* Tab buttons */}
-        <button
-          className={`px-4 py-2 mr-2 rounded-t-md ${
-            activeTab === 0 ? 'bg-blue-500 text-white' : 'bg-gray-200'
+        <div
+          className={`px-4 pt-2 mr-2 cursor-pointer ${
+            activeTab === 0 ? 'text-blue-500' : 'text-gray-500'
           }`}
           onClick={() => handleTabClick(0)}
         >
           Similar hotels nearby
-        </button>
-        <button
-          className={`px-4 py-2 rounded-t-md ${
-            activeTab === 1 ? 'bg-blue-500 text-white' : 'bg-gray-200'
+        <div
+          className={` ${
+            activeTab === 0 ? 'bg-blue-500 mt-2 h-[4px] w-full' : null
+          }`}
+        >
+        </div>
+        </div>
+        <div
+          className={`px-4 pt-2 mr-2 cursor-pointer ${
+            activeTab === 1 ? 'text-blue-500' : 'text-gray-500'
           }`}
           onClick={() => handleTabClick(1)}
         >
           Top recommended hotels
-        </button>
-        <button
-          className={`px-4 py-2 rounded-t-md ${
-            activeTab === 1 ? 'bg-blue-500 text-white' : 'bg-gray-200'
+        <div
+          className={` ${
+            activeTab === 1 ? 'bg-blue-500 mt-2 h-[4px] w-full' : null
           }`}
-          onClick={() => handleTabClick(1)}
         >
-          Top recommended hotels
-        </button>
+        </div>
+        </div>
+        <div
+          className={`px-4 pt-2 cursor-pointer ${
+            activeTab === 2 ? 'text-blue-500' : 'text-gray-500'
+          }`}
+          onClick={() => handleTabClick(2)}
+        >
+          Most popular hotels
+        <div
+          className={` ${
+            activeTab === 2 ? 'bg-blue-500 mt-2 h-[4px] w-full' : null
+          }`}
+        >
+        </div>
+        </div>
       </div>
+      <div className='h-[1px] w-full bg-gray-300'></div>
 
       {/* Tab content */}
       <div className="mt-2 p-4 border border-gray-300 rounded-b-md">
         {activeTab === 0 && <p>Tab 1 Content</p>}
         {activeTab === 1 && <p>Tab 2 Content</p>}
+        {activeTab === 2 && <p>Tab 3 Content</p>}
       </div>
     </div>
     </div>
