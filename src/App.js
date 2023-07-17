f4a076748f4077b77b0aa742f3457f0b7cf2ebc9
Symplice <intwarisymplice@gmail.com>
2023-07-17 22:12:36 +0200

    feat: adding layout page

--- src/App.js
+++ src/App.js
@@ -1,33 +1,30 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import './App.css';
-import Home from './Components/Sections/Home'
+import Home from './Components/Sections/Home';
 import Navbar from './Components/Sections/Navbar'
-import Footer from './Components/Sections/Footer';
 import NotFound from './Pages/404'
 import Car from './Pages/Car'
 import Event from './Pages/Event'
 import Homestays from './Pages/Homestays'
 import Tour from './Pages/Tour'
 import Hotel from "./Pages/Hotel";
+import PageLayout from "./Components/Sections/PageLayout";
 
 function App() {
   return (
-    <div className="App">
       <BrowserRouter>
-      <Navbar />
         <Routes>
-        <Route exact path="/" element={<Home />} ></Route>
-        <Route exact path="/hotels" element={<Hotel />} ></Route>
-        <Route exact path="/car" element={<Car />} ></Route>
-        <Route exact path="/event" element={<Event />} ></Route>
-        <Route exact path="/homestays" element={<Homestays />} ></Route>
-        <Route exact path="/tour" element={<Tour />} ></Route>
-        <Route path="*" element={<NotFound />} ></Route>
+         <Route element={<PageLayout/>}>
+        <Route path="/" element={<Home />} />
+        <Route path="/hotels" element={<Hotel />} />
+        <Route path="/car" element={<Car />}/>
+        <Route path="/event" element={<Event />} />
+        <Route path="/homestays" element={<Homestays />} />
+        <Route path="/tour" element={<Tour />} />
+        <Route path="*" element={<NotFound />} />
+        </Route>
         </Routes>
-      </BrowserRouter>
-      <Footer />
-      
-    </div>
+      </BrowserRouter> 
   );
 }
 
